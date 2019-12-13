1de4232bedc6cbfdf02eec57aa1b03064b0dbe01
Thauany <thauanysilva09@gmail.com>
2019-12-12 21:06:17 -0300

    Projeto tdd

---
+++ Aula3/Aula 14/aula/aula11.py
@@ -0,0 +1,26 @@
+
+
+
+#print pessoa['nome'])
+
+#pessoa.pop ('cpf')
+#print (f 'Ola, {pessoa ['nome']}, seja bem vindo ao nosso site)
+
+    #tupla   
+
+#salada_de_frutas = ('kiwi', 'tomate', 'tomate', 'banana', 'banana', 'banana' , 'file', 'goiaba')
+
+#salada_de_frutas.count('maça')
+
+#salada_de_frutas.count 
+
+#def soma (a,b):
+    
+    #return a + b
+
+#print (soma(3,4))
+
+numero= 4
+
+numero.__add__(3)
+
---
+++ aula21/.venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mastertech\desktop\aula21\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
